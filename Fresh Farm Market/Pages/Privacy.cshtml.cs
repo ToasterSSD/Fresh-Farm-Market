1cd650f2a482ca23bb7f6d729a3602e1974ec72c
Toaster <135201735+ToasterSSD@users.noreply.github.com>
2025-01-24 02:53:00 +0800

    Add project files.

---
+++ Fresh Farm Market/Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Fresh_Farm_Market.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
