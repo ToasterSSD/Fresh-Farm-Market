dc22a396bca7f78765ab1ee751177b298c6d7a40
Toaster <135201735+ToasterSSD@users.noreply.github.com>
2025-02-13 01:18:33 +0800

    update

--- Fresh Farm Market/wwwroot/js/site.js
+++ Fresh Farm Market/wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+// Credit card formatting
+$('input[name="Input.CreditCardNo"]').on('input', function () {
+    let value = $(this).val().replace(/\D/g, '');
+    if (value.length > 16) {
+        value = value.substr(0, 16);
+    }
+    $(this).val(value.replace(/(\d{4})(?=\d)/g, '$1 '));
+});
+
+// Phone number formatting
+$('input[name="Input.MobileNo"]').on('input', function () {
+    let value = $(this).val().replace(/\D/g, '');
+    if (value.length > 10) {
+        value = value.substr(0, 10);
+    }
+    if (value.length >= 6) {
+        value = value.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3');
+    } else if (value.length >= 3) {
+        value = value.replace(/(\d{3})(\d{0,3})/, '$1-$2');
+    }
+    $(this).val(value);
+});
