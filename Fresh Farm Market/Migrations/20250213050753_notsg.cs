72c27cfb6bbed5a3e211286c494995de8beaa69a
ToasterSSD <asherhol7@gmail.com>
2025-02-13 13:08:12 +0800

    checkpoint

---
+++ Fresh Farm Market/Migrations/20250213050753_notsg.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fresh_Farm_Market.Migrations
+{
+    /// <inheritdoc />
+    public partial class notsg : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
